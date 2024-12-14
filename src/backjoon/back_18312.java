31033e2ee348616008b53dd773618431b7f17008
ekalt <ekalt@192.168.219.109>
2024-12-14 17:37:12 +0900

    업뎃

---
+++ src/backjoon/back_18312.java
@@ -0,0 +1,36 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class back_18312 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int K = scan.nextInt();
+
+		String newK =Integer.toString(K);
+
+		int cnt=0;
+		
+		
+		StringBuilder sb = new StringBuilder();
+
+		for(int i=0; i<N+1; i++) {
+
+			for(int j=0; j<60; j++) {
+				for(int kk=0; kk<60; kk++) {
+					sb.append(i < 10 ? "0" + i : i);
+					sb.append(j < 10 ? "0" + j : j);
+					sb.append(kk < 10 ? "0" + kk : kk);
+					if(sb.indexOf(newK) != -1) {
+						cnt++;
+					}
+					sb.setLength(0);
+				}
+			}
+		}
+
+		System.out.println(cnt);
+	}
+}
---
+++ src/backjoon/back_1913.java
@@ -0,0 +1,59 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class back_1913 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int N =scan.nextInt();  
+		int target = scan.nextInt();
+		int[][] arr= new int[N][N];
+		
+		int x =0;
+		int y=0;
+		int targetX = 0;
+		int targetY = 0;
+		int Num = N*N;
+		
+		// 하, 우, 상, 좌
+		int[] dx= {1,0,-1,0};
+		int[] dy= {0,1,0,-1};
+		int nowPosition =0;
+		
+		while(Num>0) {
+			arr[x][y]=Num;
+			if(Num==target) {
+				targetX= x+1;
+				targetY= y+1;
+			}
+			Num--;
+			
+			int nx = x + dx[nowPosition];
+			int ny = y + dy[nowPosition];
+			
+			if(nx<0 || nx >=N || ny <0 || ny>=N || arr[nx][ny] != 0) {
+				nowPosition = (nowPosition+1) %4;
+				nx = x +dx[nowPosition];
+				ny = y +dy[nowPosition];
+
+			}
+			x=nx;
+			y=ny;
+		}
+		
+        // 결과 출력
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(arr[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        sb.append(targetX).append(" ").append(targetY);
+        System.out.print(sb.toString());
+		
+	}
+
+}
